f6931230e65a8cd51f5de3bcd343c36d08f3b7df
Musoxon <https://github.com/msx-git>
2022-07-11 21:04:17 +0500

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import '/solutions/rotate_array.dart';
+
+void main() {
+  rotate([1,2,3,4,5,6,7], 4);
+}
---
+++ lib/solutions/rotate_array.dart
@@ -0,0 +1,20 @@
+// Solution - 1
+void rotate(List<int> nums, int k) {
+  if (k > nums.length) {
+    k = k % nums.length;
+  }
+
+  List<int> result = List.filled(nums.length, 0);
+  for (int i = 0; i < k; i++) {
+    result[i] = nums[nums.length - k + i];
+  }
+
+  int j = 0;
+  for (int i = k; i < nums.length; i++) {
+    result[i] = nums[j];
+    j++;
+  }
+
+  List.copyRange(nums, 0, result, 0);
+  print(nums);
+}
