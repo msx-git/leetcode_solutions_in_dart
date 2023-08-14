bbc056d2748d334567534b8718a9c511e7c65b13
Musoxon <msx.tuit@gmail.com>
2023-08-14 17:34:10 +0500

    1

--- lib/1_easy/1_two_sum.dart
+++ lib/1_easy/1_two_sum.dart
@@ -1,32 +1,24 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
-    // Create an empty map to store numbers and their indices
     Map<int, int> numToIndex = {};
-    // [1, 3, 4, 6, 7, 8]
-    for (int i = 0; i < nums.length; i++) {
 
+    for (int i = 0; i < nums.length; i++) {
       int num = nums[i];
       int complement = target - num;
 
       if (numToIndex.containsKey(complement)) {
-        // Found a pair that adds up to target
-        print(numToIndex);
         return [numToIndex[complement]!, i];
       } else {
-        // Store the current number and its index in the map
         numToIndex[num] = i;
       }
-
     }
-
     return [];
   }
 }
 
 void main() {
   var s = Solution();
-
-  final target = 5;
-  print(s.twoSum([1, 3, 4, 6, 7, 8], target));
-
+  final nums = [2, 3, 4, 6, 7, 8];
+  final target = 9;
+  print(s.twoSum(nums, target));
 }
